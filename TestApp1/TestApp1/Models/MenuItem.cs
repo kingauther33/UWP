b59f06343c77170ad4a7fe260b7a9f1d6d3e536d
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-05-27 15:39:07 +0700

    Update LabS1

---
+++ TestApp1/TestApp1/Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1.Models
+{
+    class MenuItem
+    {
+        public string Name { get; set; }
+
+        public string MenuPage { get; set; }
+
+        //public MenuItem(string name, string menuPage)
+        //{
+        //    Name = name;
+        //    MenuPage = menuPage;
+        //}
+    }
+}
---
+++ TestApp1/TestApp1/Pages/Layout.xaml.cs
@@ -0,0 +1,48 @@
+﻿using TestApp1.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestApp1.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Layout : Page
+    {
+        public Layout()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Menu_Loaded(object sender, RoutedEventArgs e)
+        {
+            //List<MenuItem> items = new List<MenuItem>();
+            //items.Add(new MenuItem { Name = "Home", MenuPage = "home" });
+            //items.Add(new MenuItem { Name = "Category", MenuPage = "category" });
+            MN.Items.Add(new MenuItem { Name = "Home", MenuPage = "home" });
+            MN.Items.Add(new MenuItem { Name = "Category", MenuPage = "category" });
+        }
+
+        private void ListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MenuItem item = MN.SelectedItem as MenuItem;
+
+            switch (item.MenuPage)
+            {
+                case "home": 
+                    {
+                        MainFrame.Navigate(typeof(Pages.Home), "Day la trang chu");
+                        break;
+                    }
+                case "category":
+                    {
+                        MainFrame.Navigate(typeof(Pages.Category), "Day la trang category");
+                        break;
+                    }
+            }
+        }
+    }
+}
