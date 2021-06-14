e9b409768e0a35c6e2199b878edcf8e99e532391
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-06-14 21:31:50 +0700

    Update Practical

--- Practical/Adapters/SQLiteHelper.cs
+++ Practical/Adapters/SQLiteHelper.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLitePCL;
+using Practical.Models;
+
 
 namespace Practical.Adapters
 {
     class SQLiteHelper
     {
+        private readonly string dbName = "exam.db";
+
+        private static SQLiteHelper sQLiteHelper;
+
+        public static SQLiteHelper GetInstance()
+        {
+            if (sQLiteHelper == null)
+            {
+                sQLiteHelper = new SQLiteHelper();
+            }
+            return sQLiteHelper;
+        }
+
+        private SQLiteHelper()
+        {
+            string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, dbName);
+            sQLiteConnection = new SQLiteConnection(path); // tao db;
+            CreateCartTable();
+        }
+
+        public SQLiteConnection sQLiteConnection { get; private set; }
+
+        public void CreateCartTable() // tao bang cart
+        {
+            var sql_txt = @"CREATE TABLE IF NOT EXISTS User(id integer primary key, password varchar(200))";
+            var statement = sQLiteConnection.Prepare(sql_txt);
+            statement.Step();
+        }
     }
 }
--- Practical/Pages/Question2.xaml.cs
+++ Practical/Pages/Question2.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Practical.Adapters;
+using Practical.Models;
+using SQLitePCL;
+using Windows.UI;
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +31,40 @@ namespace Practical.Pages
         public Question2()
         {
             this.InitializeComponent();
+            AddToDatabase(1, "iloveyou123");
+            AddToDatabase(2, "iloveyou1234");
+            AddToDatabase(3, "iloveyou12345");
+        }
+
+        public void AddToDatabase(int id, string password)
+        {
+            SQLiteHelper qLiteHelper = SQLiteHelper.GetInstance();
+            SQLiteConnection sQLiteConnection = qLiteHelper.sQLiteConnection;
+            string sql_txt = "insert into User (id,password) values(?,?)";
+            var statement = sQLiteConnection.Prepare(sql_txt);
+            statement.Bind(1, id);
+            statement.Bind(2, password);
+            var rs = statement.Step();
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteHelper qLiteHelper = SQLiteHelper.GetInstance();
+            SQLiteConnection sQLiteConnection = qLiteHelper.sQLiteConnection;
+            string sql_txt = "select * from User where (id = ? and password = ?)";
+            var statement = sQLiteConnection.Prepare(sql_txt);
+            statement.Bind(1, userBox.Text);
+            statement.Bind(2, passwordBox.Password.ToString());
+            if (SQLiteResult.ROW == statement.Step())
+            {
+                successFail.Text = "Login successfully";
+                successFail.Foreground = new SolidColorBrush(Colors.Green);
+            } else
+            {
+                successFail.Text = "Login failed!";
+                successFail.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
         }
     }
 }
